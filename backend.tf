ad1b12ce1e5956a26cdf4c5544b0a078d04d7e08
Abdul Samad <abdulsamad@gmail.com>
2022-08-09 02:18:04 +0500

    remote backend

--- backend.tf
+++ backend.tf
@@ -1,13 +1,16 @@
 # The block below configures Terraform to use the 'remote' backend with Terraform Cloud.
 # For more information, see https://www.terraform.io/docs/backends/types/remote.html
+
+
+# Using a single workspace:
 terraform {
   backend "remote" {
-    organization = "{{Samad_Malik}}"
+    hostname = "app.terraform.io"
+    organization = "asamad"
 
     workspaces {
-      name = "{{ASamad_Learning_Terraform}}"
+      name = "terraform-learning"
     }
   }
-
-  required_version = ">= 0.14.0"
 }
+
